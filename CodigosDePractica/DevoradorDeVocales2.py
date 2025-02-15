4832e91457123ada3adcc30bbf29895be0e2cedd
RamiroSGaspar <gapsar.sebastian@gmail.com>
2025-02-15 17:00:38 -0300

    Nuevos elementos

--- CodigosDePractica/DevoradorDeVocales2.py
+++ CodigosDePractica/DevoradorDeVocales2.py
@@ -1,3 +1,17 @@
+'''
+Devorador de vocales ver2
+
+Explicación:
+
+La varibale "palabra" tiene la misma funcion que en la anterior ver, pero
+se agrega una nueva variable con una cadena vacia, "LetrasAcumuladas".
+
+Se inicia un bucle for, que la funcionalidad es la misma que en la anterior version
+pero en el cuerpo se utiliza una sola condicion, si la variable de control no contiene
+las vocales entonces se acumulan en la variable que se creo para ir mostrando despues
+en pantalla, por que ahora la palabra sin las vocales se van a mostrar en una sola linea
+en la salida
+'''
 palabra = input("Ingresa una variable para quitarle las vocales: ").upper()
 LetrasAcumuladas = ""
 
