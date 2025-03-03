291fe84dcc49f4b6b4a5960c7ed5cb5a02b718ae
RamiroSGaspar <gapsar.sebastian@gmail.com>
2025-03-03 17:23:08 -0300

    Nuevos elementos

--- CodigosDePractica/a.py
+++ CodigosDePractica/a.py
@@ -3,3 +3,4 @@ var_right = var >> 1
 var_left = var << 2
 
 print(var, var_left, var_right)
+print(format(var & 0xFFFF, '016b'), format(var_left & 0xFFFF, '016b'), format(var_right & 0xFFFF, '016b'))
